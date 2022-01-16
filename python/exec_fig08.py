Fix rotation angles and ray tracing. Add c++ modules.

python/exec_fig08.py
@@ -1,11 +1,11 @@
-#!/usr/bin/env python3.8
+#!/usr/bin/env python3
 
 import numpy as np
 from pdb import set_trace as stop
 import time as tm
 
 import apy_utils as uts
-import mirror_lib_v03 as mrr
+import pymirrors as mrr
 
 #TO BE MOVED WHEN SATISFACTORY RESULTS ARE OBTAINED
 from os.path import exists
@@ -13,53 +13,32 @@ from os import mkdir
 import matplotlib.pyplot as pl
 pl.ioff()
 
-idealc=False#True
-cdust=True
-plotsec=False#True#False
-printout=True#True
-savedata=True#False#False
-overwrite=True#False#False
+nthread = 8
 
-odirseed = 'results_202110_v03/'
+idealc = False
+cdust = True
+plotsec = False
+printout = True
+savedata = True
+overwrite = True
+
+odirseed = 'results_2022_v05/'
 
 #
 telescope = 'gtc'
-#telescope = 'eelt'
 #
 methods = []
-#methods.append('equal')
 methods.append('azimuthal')
 methods.append('lineal')
 methods.append('random')
 methods.append('symmetric')
-#methods = ['lineal']
 #
 print('Telescope chosen!')
 alpha = 0.
 x_alpha = 0.
 y_alpha = 0.
 print('Orientation chosen!')
-# TMP COMT:: ><#
-# TMP COMT:: ><#period = 365 #798.#365.24 * 2.
-# TMP COMT:: ><deltat= 10.#365./36.
-# TMP COMT:: ><#2. * 365.24 #TIME NEEDED FOR CHANGING ALL THE MIRRORS IN DAYS
-# TMP COMT:: ><cleandust = 390.      # DUST CLEANING TIME FREQUENCY [in days?]
-# TMP COMT:: ><tstep = 1. # SIMULATION TIME STEP   $NUMBER OF TIMESTEPS FOR EACH MIRROR
-# TMP COMT:: ><#
-# TMP COMT:: ><tlong = 370#365*2+6#798. # Simulated days
-# TMP COMT:: ><print('Time parameters chosen!')
-# TMP COMT:: ><#lamb = 3.0e3    #ANGSTROM
-# TMP COMT:: ><lambs = [5556.]#[3500.,5500.,10500.,15500.]#(np.arange(8)+3.)*1000. 
-# TMP COMT:: ><print('Wavelength chosen!')
-# TMP COMT:: ><nums = [360]#(np.arange(100,dtype=np.int32)+1)*10
 print('Number of rays per segment chosen!')
-
-
-#lambs=[5000.]#(np.arange(8)+1.)*1000.+2000.
-#lambs=np.linspace(2100.,24000.,219+1)
-#nums = (np.arange(20)+1) * 36
-#nums=[48]
-#nums=3*(np.arange(80)+1)*4
 
 if (telescope=='eelt'):
   deltat = 1.
@@ -88,14 +67,11 @@ elif (telescope=='gtc'):
     cleandust = 1.      # DUST CLEANING TIME FREQUENCY [in days?]
 
 nums = [4096]
-#lambs = [6500.]
+
+################################################################################
 
 if (not exists(odirseed)):
   mkdir(odirseed)
-
-#True
-
-################################################################################
 
 for method in methods:
 
@@ -108,7 +84,6 @@ for method in methods:
       if (not exists(outdir)):
         mkdir(outdir)
       
-      # Posibles necesidades: tstep, tlong, cleansdust, period, tlong
       oname1 = '%s/full_mueller_matrix_w%.2f_Dt%.2f_St%.2f_%08i_%010.2f_%i.fits' \
             % (outdir, lamb, deltat, tstep, tlong, cleandust, mltch, )
       oname2 = '%s/avg_mueller_matrix_w%.2f_Dt%.2f_St%.2f_%08i_%010.2f_%i.fits' \
@@ -130,26 +105,17 @@ for method in methods:
         t1 = tm.time()
         segments = mrr.primary(teles, method, tstep, tlong, cleandust \
             , deltat=deltat, ideal=idealc, multiplechange=mltch)
-      #, period=period)
-        #><import matplotlib.pyplot as pl
-        #><pl.ion()
-        #><pl.figure(1)
-        #><pl.clf() ; pl.imshow(segments.time_map[:,:,0].T) ; pl.show()
-        #><#stop()
-        #><pl.figure(2)
-        #><pl.clf() ; pl.imshow(segments.time_map[:,:,1].T) ; pl.show()
-        #><stop()
         
         t2 = tm.time()
         materials = mrr.dirt_timing(lamb, segments.time_map)
         
         t3 = tm.time()
-        #mrr.get_geometry(teles, beam, secondary, segments, osecondary=False)
-        mrr.get_geometry(teles, beam, secondary, segments, osecondary=plotsec)
+        mrr.get_geometry(teles, beam, secondary, segments, osecondary=plotsec \
+            , nthreads=nthread)
       
         t4 = tm.time()
-        avg_mat,mat,npts_seg = mrr.get_mueller_time(segments, materials, cdust=cdust)
-        to_save_mat = mat * 1.
+        avg_mat,mat,npts_seg = mrr.get_mueller_time(segments, materials \
+            , cdust=cdust, nthreads=nthread)
         
         t5 = tm.time()
         
@@ -165,7 +131,7 @@ for method in methods:
           uts.writefits_v3(mat, oname1, overwrite=1)
           uts.writefits_v3(avg_mat, oname2, overwrite=1)
           uts.writefits_v3(npts_seg, oname3, overwrite=1)
-        to_save_mat = mat * 1.
+
         if (printout==True):
           print(avg_mat)
 